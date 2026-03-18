01a98dc95a8a42fc2b99eed7e1374ab64620960b
Baidu <baidunano.account@proton.me>
2026-03-18 14:32:52 +0700

    Init

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "template.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << '\n';
+
+    Template::greet();
+    return 0;
+}
---
+++ src/template.cpp
@@ -0,0 +1,6 @@
+#include "template.hpp"
+#include <iostream>
+
+void Template::greet() {
+    std::cout << "Hello class" << '\n';
+}
